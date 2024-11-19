caaac2c42a75e95063ce1aebb07ef8fb2df43fb4
gauravrana12 <gauravp1335@gmail.com>
2024-11-19 22:01:21 +0530

    progress

--- src/Components/Postpaid.jsx
+++ src/Components/Postpaid.jsx
@@ -24,7 +24,8 @@ const Postpaid = () => {
           <div>
             <div>
               <div className="imglogo">
-                <img className="flaticon"
+                <img
+                  className="flaticon"
                   src="https://cdn-icons-png.flaticon.com/128/12689/12689828.png"
                   alt="logo"
                 />
@@ -34,28 +35,47 @@ const Postpaid = () => {
               </p>
             </div>
             <div>
-            <div className="imglogo">
-              <img className="flaticon"
-                src="https://cdn-icons-png.flaticon.com/128/2652/2652181.png"
-                alt="logo"
-              />
+              <div className="imglogo">
+                <img
+                  className="flaticon"
+                  src="https://cdn-icons-png.flaticon.com/128/2652/2652181.png"
+                  alt="logo"
+                />
               </div>
               <p>
                 Shop across <br /> 20,000 apps
               </p>
             </div>
             <div>
-            <div className="imglogo">
-              <img className="flaticon"
-                src="https://cdn-icons-png.flaticon.com/128/664/664460.png"
-                alt="logo"
-              />
+              <div className="imglogo">
+                <img
+                  className="flaticon"
+                  src="https://cdn-icons-png.flaticon.com/128/664/664460.png"
+                  alt="logo"
+                />
               </div>
               <p>
                 Credit limit upto <br /> ₹100,000
               </p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="Ins">
+        <div>
+          <h3>Insurance</h3>
+          <h2>
+            Effective insurances for all <br /> your risks
+          </h2>
+          <h4> Explore more on the Ola app →</h4>
+        </div>
+        <div>
+          <img
+            src="https://s3-ap-southeast-1.amazonaws.com/ola-prod-website/insure-trips.svg"
+            alt="1st"
+          />
+          <img src="https://s3-ap-southeast-1.amazonaws.com/ola-prod-website/insure-daily.svg" alt="2nd" />
+          <img src="https://s3-ap-southeast-1.amazonaws.com/ola-prod-website/insure-hospicash.svg" alt="3rd" />
         </div>
       </div>
     </div>
